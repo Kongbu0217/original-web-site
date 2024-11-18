2c043bcf5cf8728df4d40e135736ffac893ef676
Kongbu0217 <meeko_aus@icloud.com>
2024-11-18 15:19:22 +0800

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//   https://scrollrevealjs.org/guide/customization.html
+//　ふわっとmove加工
+
+ScrollReveal({
+    reset: true, 
+    distance: "30px" 
+});
+
+ScrollReveal().reveal('h3', {
+    delay:300,
+    origin: "top"
+});
+
+ScrollReveal().reveal(".story p, .ex1 p, .ex2 p, .ex3 p, .want p, .question p ",  {
+    delay:400,
+    origin: "bottom"
+});
+
+//文字のふわっとmove完了
+
+//画像の回転 動かない
