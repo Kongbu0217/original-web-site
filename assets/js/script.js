1b3b3946605ce1590f6f2a17ceeb2077c75e3c60
Kongbu0217 <meeko_aus@icloud.com>
2024-11-19 13:59:38 +0800

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,8 @@ ScrollReveal().reveal('h3', {
 });
 
 ScrollReveal().reveal(".story p, .ex1 p, .ex2 p, .ex3 p, .want p, .question p ",  {
-    delay:400,
+    delay:300,
     origin: "bottom"
 });
 
 //文字のふわっとmove完了
-
-//画像の回転 動かない
